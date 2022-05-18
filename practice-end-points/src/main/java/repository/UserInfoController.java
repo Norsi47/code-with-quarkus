8dd58e1c7888f13824b1ec195843f5b7baca1736
Norsi47 <briggsnorsi@yahoo.com>
2022-05-17 22:07:24 -0400

    adding more end points

--- practice-end-points/src/main/java/repository/UserInfoController.java
+++ practice-end-points/src/main/java/repository/UserInfoController.java
@@ -5,6 +5,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -33,6 +34,24 @@ public class UserInfoController {
         //this is in an array list to let it continue to create more info
         List<UserInfoEntity> userInfoEntityList = UserInfoEntity.listAll();
         return Response.ok(userInfoEntityList).build();
+    }
+    //to get by usersName
+    @GET
+    @Path("/getByUsersName/{usersName}")
+    public Response getByUsersName(@PathParam("usersName") String usersName) {
+        List<UserInfoEntity> userInfoEntityList = UserInfoEntity.list("SELECT u FROM" +
+                "UserInfoEntity u WHERE u.usersName = ?1 ORDER BY DESC" , usersName);
+        return Response.ok(userInfoEntityList).build();
+    }
+
+    //find by ID
+    @GET
+    @Path("/getById/{id}")
+    public Response getById(@PathParam("id") Long id) {
+        return UserInfoEntity.findByIdOptional(id)
+                .map(users -> Response.ok(users).build())
+                .orElse(Response.status(Response.Status.NOT_FOUND).build());
+
     }
 
 }
